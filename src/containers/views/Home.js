34c81ca6665a787a8e3f5b4e9c2971fa27cfd3bb
Shaoyi Lin <alex.lin@verbinteractive.com>
2021-11-09 20:41:20 -0400

    Build home page

--- src/containers/views/Home.js
+++ src/containers/views/Home.js
@@ -1,10 +1,51 @@
 import React from 'react';
-import '../../styles/views/home.scss'
+import '../../styles/views/home.scss';
+import {motion} from 'framer-motion';
+import HomeC from '../../assets/icons/homeC.svg';
+import HomeA from '../../assets/icons/homeA.svg';
+import HomeD from '../../assets/icons/homeD.svg';
+import Home9 from '../../assets/icons/home9.svg';
 
 const Home = () => (
-    <div className="home">
-      <h1>Cadd9</h1>
+  <div className="home">
+    <div className="home_header">
+      <motion.img
+        src={HomeC}
+        alt="cadd9_letter_c"
+        className="home_letter home_c"
+        whileHover={{y: -40}}
+        transition={{duration: 0.5}}
+      />
+      <motion.img
+        src={HomeA}
+        alt="cadd9_letter_a"
+        className="home_letter home_a"
+        whileHover={{y: 40}}
+        transition={{duration: 0.5}}
+      />
+      <motion.img
+        src={HomeD}
+        alt="cadd9_letter_d"
+        className="home_letter home_d"
+        whileHover={{y: -40}}
+        transition={{duration: 0.5}}
+      />
+      <motion.img
+        src={HomeD}
+        alt="cadd9_letter_d"
+        className="home_letter home_dd"
+        whileHover={{y: 40}}
+        transition={{duration: 0.5}}
+      />
+      <motion.img
+        src={Home9}
+        alt="cadd9_letter_9"
+        className="home_letter home_9"
+        whileHover={{y: 40}}
+        transition={{duration: 0.5}}
+      />
     </div>
-)
+  </div>
+);
 
 export default Home;
