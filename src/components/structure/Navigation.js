6d69d20f39bb4bfaee8e4d14d537747957b65be4
Shaoyi Lin <alex.lin@verbinteractive.com>
2021-11-10 01:50:28 -0400

    Add nav bar for mobile view

--- src/components/structure/Navigation.js
+++ src/components/structure/Navigation.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import {motion} from 'framer-motion'
+import React, {useState} from 'react';
+import {motion} from 'framer-motion';
 import {steps} from '../../configuration/router';
 import {Link} from 'react-router-dom';
 
 import '../../styles/structure/navigation.scss';
 
-const Navigation = () => (
-  <nav className="nav-bar">
-    {steps.map((item, index) => (
-      <Link className="nav-item" key={index} to={item.value}>
-        <motion.p whileHover={{letterSpacing: '15px'}} transition={{duration: 0.5}}>{item.title}</motion.p>
-      </Link>
-    ))}
-  </nav>
-);
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <nav className={`nav-bar ${isOpen ? 'open-nav-bar' : ''}`}>
+        {steps.map((item, index) => (
+          <Link className="nav-item" key={index} to={item.value}>
+            <motion.p
+              whileHover={{letterSpacing: '15px'}}
+              transition={{duration: 0.5}}
+            >
+              {item.title}
+            </motion.p>
+          </Link>
+        ))}
+      </nav>
+
+      <i className="fas fa-bars menu-bar-item fa-2x" onClick={() => setIsOpen(!isOpen)}></i>
+    </>
+  );
+};
 
 export default Navigation;
