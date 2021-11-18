6356b8f6740e5770ea1d1221413bb1add8b685c2
Shaoyi Lin <alex.lin@verbinteractive.com>
2021-11-18 09:55:14 -0400

    Add footer content

--- src/components/structure/Footer.js
+++ src/components/structure/Footer.js
@@ -9,7 +9,10 @@ const Footer = () => {
       <p className="font-heading-english f-24">
         &copy; Cadd9 <span className="font-heading">艺术音乐中心</span> {`${currentYear}`}
       </p>
-      <a href="http://beian.miit.gov.cn" className="web-id-number">京ICP备2021035395</a>
+      <div className="flex flex-align-center flex-column-md-max">
+        <p className="mb-0 f-11 leading-normal pr-2">Cadd9 音乐艺术中心是北京妙手天成教育咨询有限公司旗下品牌</p>
+        <a href="http://beian.miit.gov.cn" className="web-id-number f-11">京ICP备2021035395</a>
+      </div>
     </div>
 )}
 
