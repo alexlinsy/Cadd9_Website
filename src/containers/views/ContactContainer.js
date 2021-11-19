37654cca264de37aa8e494270b50276ef3157731
Shaoyi Lin <alex.lin@verbinteractive.com>
2021-11-19 09:52:47 -0400

    add qr code

--- src/containers/views/ContactContainer.js
+++ src/containers/views/ContactContainer.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ScrollAnimation from 'react-animate-on-scroll';
 import {LazyLoadImage} from 'react-lazy-load-image-component';
 import {contactData} from '../../data/contact-data';
-import WeChatQRCode from '../../assets/images/weChat-qr-code.png'; 
+import WeChatQRCode from '../../assets/images/weChat-qr-code.png';
+import WeChatQRCode2 from '../../assets/images/weChat-qr-code2.png';
 import MapContainer from '../../components/elements/MapContainer';
 import { motion } from 'framer-motion';
 import ContactForm from '../../components/elements/ContactForm';
@@ -42,7 +43,10 @@ const ContactContainer = () => {
         duration={3}
         className="flex flex-column mt-2 py-2 border-top"
       >
-        <LazyLoadImage src={WeChatQRCode} alt="QRCode" className="qr-code-image mt-5"/>
+        <div className="flex mt-5 qr-code-section">
+          <LazyLoadImage src={WeChatQRCode} alt="QRCode" className="qr-code-image"/>
+          <LazyLoadImage src={WeChatQRCode2} alt="QRCode another" className="qr-code-image ml-5" />
+        </div>
         <a href="tel:+8617813206997" className="f-20 font-heading mb-1 contact-tel mt-2">Tel: {tel}</a>
         <a href="mailto:Cadd9music@163.com" className="f-20 font-heading-english mb-0 contact-email">{email}</a>
       </ScrollAnimation>
