2b388c07b89d4761cf1f5b98bb85483036cf9815
Shaoyi Lin <alex.lin@verbinteractive.com>
2021-11-30 00:04:22 -0400

    Add news section

---
+++ src/components/elements/NewsCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  LazyLoadImage,
+  trackWindowScroll,
+} from 'react-lazy-load-image-component';
+
+import '../../styles/elements/news-card.scss';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+const NewsCard = ({image, date, title, content, url, scrollPosition}) => {
+  const openInNewTab = url => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
+    if (newWindow) newWindow.opener = null;
+  };
+  return (
+    <div
+      className="flex flex-column news-card mb-10"
+      onClick={() => openInNewTab(url)}
+    >
+      <LazyLoadImage
+        src={image}
+        alt="news"
+        className="full-width"
+        effect="blur"
+        scrollPosition={scrollPosition}
+      />
+      <p className="font-heading mt-5 mb-3 f-24">{date}</p>
+      <p className="font-heading mb-0 f-24">{title}</p>
+      <div
+        className="font-heading mb-0 mt-5 leading-normal news-content"
+        dangerouslySetInnerHTML={{__html: content}}
+      ></div>
+    </div>
+  );
+};
+
+export default trackWindowScroll(NewsCard);
--- src/containers/views/NewsContainer.js
+++ src/containers/views/NewsContainer.js
@@ -1,11 +1,39 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import moment from 'moment';
+import HeaderSection from '../../components/elements/HeaderSection'
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
 import '../../styles/views/news.scss';
+import NewsCard from '../../components/elements/NewsCard';
 
-const NewsContainer = () => (
-    <div className="news-container">
-      News
+
+const NewsContainer = () => {
+  const momentDate = moment("2021-11-29T13:57:25-04:00").format("MM.DD.YY");
+  console.log(momentDate);
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      'https://public-api.wordpress.com/rest/v1.1/sites/cadd9music.wordpress.com/posts'
+    )
+      .then(res => res.json())
+      .then(res => setPosts(res.posts));
+  }, []);
+
+  return (
+    <div className="news-container px-10">
+      <HeaderSection title="新闻" borderRequired />
+      <div className="news-section mt-8 flex flex-wrap flex-space-between flex-column-md-max">
+        {posts && posts.map((post, index) => {
+           const {featured_image, date, title, content, tags} = post;
+          return (
+            <LazyLoadComponent key={index}>
+              <NewsCard image={featured_image} date={moment(date).format("MM.DD.YY")} title={title} content={content} url={Object.keys(tags)[0]} />
+            </LazyLoadComponent>
+          );
+       })}
+      </div>
     </div>
-)
+  );
+};
 
 export default NewsContainer;
