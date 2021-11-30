f802782fcd6efc245faa719df93d4f14216b8b41
Shaoyi Lin <alex.lin@verbinteractive.com>
2021-11-30 19:02:10 -0400

    Add text to partners container

--- src/containers/views/PartnersContainer.js
+++ src/containers/views/PartnersContainer.js
@@ -11,6 +11,11 @@ const PartnersContainer = () => {
   return (
     <div className="px-10 partners-container">
       <HeaderSection title={headerBusiness} borderRequired />
+      <p className="mt-5 mb-0 partner-intro-text leading-normal flex flex-column flex-justify-center text-center">
+        <span>创始人与其余Cadd9导师从未停止过在音乐产业的征途，</span>
+        <span>以下合作方与院校都与我们有良好合作关系，</span>{' '}
+        <span>音乐教育是Cadd9一半的家的话，</span>音乐产业就是Cadd9的另一半家。
+      </p>
       <div className="mt-5 flex flex-wrap flex-justify-center partners-icons">
         {partnersBusiness.map((item, index) => (
           <PartnerBox partner={item} key={index} />
