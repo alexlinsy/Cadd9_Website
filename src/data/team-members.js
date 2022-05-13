76a8e511ba6d78fe92c1d0fc068103353d8ee8f8
Alexlinsy <alexlinsy@live.com>
2022-05-13 11:06:52 -0600

    Change team page content

--- src/data/team-members.js
+++ src/data/team-members.js
@@ -7,13 +7,16 @@ import avatar6 from '../assets/avatars/avatar6.png';
 import avatar7 from '../assets/avatars/avatar7.png';
 import avatar8 from '../assets/avatars/avatar8.png';
 import avatar9 from '../assets/avatars/avatar9.png';
-import avatar10 from '../assets/avatars/avatar10.png';
 import avatar11 from '../assets/avatars/avatar11.png';
 import avatar12 from '../assets/avatars/avatar12.png';
 import avatar13 from '../assets/avatars/avatar13.png';
 import avatar14 from '../assets/avatars/avatar14.png';
 import avatar15 from '../assets/avatars/avatar15.png';
 import avatar16 from '../assets/avatars/avatar16.png';
+import avatar17 from '../assets/avatars/avatar17.png';
+import avatar18 from '../assets/avatars/avatar18.png';
+import avatar19 from '../assets/avatars/avatar19.png';
+import avatar20 from '../assets/avatars/avatar20.png';
 
 export const teamMembers = {
   header: '我们的团队',
@@ -26,99 +29,90 @@ export const teamMembers = {
         '毕业于美国伯克利音乐学院，现代编曲制作专业，主修乐器声乐。同是词曲创作人，歌手，制作人。获多次唱片公司，腾讯网易等各平台等作曲类奖项，长期为艺人歌手创作制作。对于音乐产业和音乐教育极为熟知，辅导过多名备考生，均顺利考入伯克利音乐学院。他自己的话“我不会自称自己是一个老师，我只希望成为音乐路上最能帮助和启发大家的人。”',
     },
     {
-      name: 'MC',
-      title: ' ',
-      image: avatar2,
-      intro:
-        '毕业于伯克利音乐学院，获半额奖学金，音乐商业管理专业，主修乐器打击乐。参办那英、Twins、GAI周延、李云迪、理查德·克莱德曼等多个艺人的北美巡演活动，在艺人/演出经纪方面有丰富经验。在GOCAA全球钢琴大赛中担任波士顿地区行政负责人。也曾于中央芭蕾舞团交响乐团、国家大剧院交响乐团打击乐声部中参演多场交响乐、芭蕾舞剧，及歌剧演出。“Love what you do，and do what you love.”',
-    },
-    {
-      name: 'Leng Leng',
-      title: ' ',
-      image: avatar3,
-      intro:
-        '美国伯克利音乐学院爵士钢琴表演专业。致力于探索流行乐、爵士乐、 city pop、与电子⻛格音乐的融合。师从爵士乐大师Yoko Miwa、Doug Johnson、等。结合自由的风格去帮助所有热爱音乐的学生。',
-    },
-    {
-      name: 'JZ',
-      title: ' ',
-      image: avatar4,
-      intro:
-        '毕业于美国伯克利音乐学院，Cadd9教学副主管，电影配乐与现代音乐制作双专业，主修乐器钢琴。Artist Creativity奖学金获得者，其作品做为伯克利年度专业代表作品在毕业典礼播放。在游戏、电影电视剧，纪录片的配乐、音效设计、录音、制作上具有丰富经验。参与作品包括游戏音乐《Astry》、《Project Origin》、《Karisvale》，纪录片《A Doctor’s Journey》、《LionFamily》等。长期辅导多名学生乐理，钢琴，制作，电影配乐。',
-    },
-    {
-      name: 'Tanni',
-      title: ' ',
-      image: avatar5,
-      intro:
-        '钢琴专业，Cadd9古典部负责人，毕业于美国新英格兰音乐学院。被CBC音乐电视台评为“加拿大十大Piano Hero”，并获“2020全美施坦威最佳钢琴老师”称号。Crescendo 国际比赛、Golden Classical Awards、Launuadiere钢琴比赛中获第一名及最高奖项。以及曾先后与卡普林斯基，邓泰山，Ronan O’hora, Jeffery Cohen， Benjamin Pasternak等顶级大师演奏⼤师班，拥有多年教学经验。',
-    },
-    {
-      name: 'Ray',
-      title: ' ',
-      image: avatar6,
-      intro:
-        'YG娱乐、各大音乐厂牌，及电视节目资深声乐老师，具备多年声乐教学经验。英国金斯顿大学音乐表演硕士，北京大学艺术理论在读博士，本科毕业于韩国东亚媒体艺术学院音乐制作专业。在声乐、音乐史、乐队、音乐制作、音乐理论中均有丰富的实践与研究经验。',
-    },
-    {
       name: 'FF',
-      title: ' ',
+      title: '',
       image: avatar7,
       intro:
         '拉美吉他演奏家，美国伯克利音乐学院全额奖学金获得者，同为制作人，编曲人，词曲作者。曾三年连续荣获三次格莱美拉丁演奏奖项，发行过多张个人专辑及单曲，在各大国际音乐节均有演出，并常年巡演于欧洲。目前也同时致力于音乐教育。',
     },
     {
+      name: 'Katherine',
+      title: '',
+      image: avatar19,
+      intro: 'Katherine 旅美长笛演奏家，美国波士顿音乐学院长笛硕士，奥伯林音乐学院本科。客席长笛演奏员，北京交响乐团，中国交响乐团随团与中外多个著名指挥家、演奏 家合作演出，包括谭利华、郑小瑛、谭盾、李心草、林大叶、吕思清、戴玉强等音乐家。2019年9月参加新中国成立70周年晚会录制，2021年作为艺术顾问评委参加第十三届国际华人艺术节，2021年8月作为导师参加昆明国际音乐节。辅导过很多长笛考生考取国外院校。'
+    },
+    {
+      name: 'Jonas',
+      title: '',
+      image: avatar20,
+      intro: 'Jonas，歌手、声乐老师，伯克利音乐学院，中传双学士学位。师从SLS认证、伯克利音乐学院声乐教学。擅长流行、R&B、放克、爵士等多种演唱风格。参与多个卫视综艺节目和跨年晚会录制，合作包括王源等艺人。所教授的学生均成功申请上包括伯克利在内的海外音乐院校本科及硕士。'
+    },
+    {
+      name: 'JZ',
+      title: '',
+      image: avatar4,
+      intro:
+        '毕业于美国伯克利音乐学院，Cadd9教学副主管，电影配乐与现代音乐制作双专业，主修乐器钢琴。Artist Creativity奖学金获得者，其作品做为伯克利年度专业代表作品在毕业典礼播放。在游戏、电影电视剧，纪录片的配乐、音效设计、录音、制作上具有丰富经验。参与作品包括游戏音乐《Astry》、《Project Origin》、《Karisvale》，纪录片《A Doctor’s Journey》、《LionFamily》等。长期辅导多名学生乐理，钢琴，制作，电影配乐。',
+    },
+    {
+      name: 'XTF',
+      title: '',
+      image: avatar11,
+      intro:
+        '毕业于美国伯克利音乐学院，professional music 专业，专读CWP、MPE，与Performance三个专业。是一位职业键盘手，编曲人，制作人，作曲家。参演哈尔滨国际冰雪节，哈尔滨街头音乐节（乐队金奖），国际bass音乐节等多个音乐节活动。参与2012超级女声节目，2012“尤雅哈尔滨‘魅力一生’演唱会"，2014“为爱向前冲”节目录制。15年担任辽宁爱乐乐团键盘手，17年任全国打击乐比赛评委及键盘手。',
+    },
+    {
+      name: 'Ray',
+      title: '',
+      image: avatar6,
+      intro:
+        'YG娱乐、各大音乐厂牌，及电视节目资深声乐老师，具备多年声乐教学经验。英国金斯顿大学音乐表演硕士，北京大学艺术理论在读博士，本科毕业于韩国东亚媒体艺术学院音乐制作专业。在声乐、音乐史、乐队、音乐制作、音乐理论中均有丰富的实践与研究经验。',
+    },
+    {
       name: 'Daniel',
-      title: ' ',
+      title: '',
       image: avatar8,
       intro:
         '伯克利音乐学院亚太区招生主任，Cadd9咨询伙伴，致力于与伯克利校方制定全球发展战略。毕业于意大利那不勒斯东方大学，文化硕士学位。在意大利那不勒斯出生长大，曾在中国生活。 同时Daniel也是一位吉他手和词曲作者，其大部分歌曲在旅途中创作，多在摇滚乐风格中融合进他的个人故事。',
     },
     {
       name: 'MA',
-      title: ' ',
+      title: '',
       image: avatar9,
       intro:
         '于2015年加入美国新英格兰音乐学院，在美国大都会歌剧院中多次亮相。2012格莱美最佳歌剧录音奖，及多个国际歌剧比赛奖项获得者。本科毕业于伊士曼音乐学院学习歌剧表演专业，并获新英格兰音乐学院艺术家文凭。她的国际代表作品包括在意大利米兰斯卡拉大剧院的《意大利的伊莎⻉拉》中饰演的伊莎⻉拉；在都灵大剧院的 Il Trovatore 饰演的Azucena ，以及在罗⻢歌剧院的卡⻔⻆色等。',
     },
     {
-      name: 'YX',
-      title: ' ',
-      image: avatar10,
+      name: 'Tanni',
+      title: '',
+      image: avatar5,
       intro:
-        '毕业于美国伯克利音乐学院，音乐治疗专业，辅修心理学，曾在美国波士顿，特殊儿童教育、精神康复及肿瘤康复等领域进行音乐治疗临床实践。并于世界顶尖的综合医院 - 麻省总医院Massachusetts General Hospital(全世界前三的医院)完成了毕业临床实习，通过了美国音乐治疗职业考试，获得注册音乐治疗师资格证。辅导过多名备考生，均顺利考入伯克利音乐学院。',
-    },
-    {
-      name: 'XTF',
-      title: ' ',
-      image: avatar11,
-      intro:
-        '毕业于美国伯克利音乐学院，professional music 专业，专读CWP、MPE，与Performance三个专业。是一位职业键盘手，编曲人，制作人，作曲家。参演哈尔滨国际冰雪节，哈尔滨街头音乐节（乐队金奖），国际bass音乐节等多个音乐节活动。参与2012超级女声节目，2012“尤雅哈尔滨‘魅力一生’演唱会"，2014“为爱向前冲”节目录制。15年担任辽宁爱乐乐团键盘手，17年任全国打击乐比赛评委及键盘手。',
+        '钢琴专业，Cadd9古典部负责人，毕业于美国新英格兰音乐学院。被CBC音乐电视台评为“加拿大十大Piano Hero”，并获“2020全美施坦威最佳钢琴老师”称号。Crescendo 国际比赛、Golden Classical Awards、Launuadiere钢琴比赛中获第一名及最高奖项。以及曾先后与卡普林斯基，邓泰山，Ronan O’hora, Jeffery Cohen， Benjamin Pasternak等顶级大师演奏⼤师班，拥有多年教学经验。',
     },
     {
       name: 'XYW',
-      title: ' ',
+      title: '',
       image: avatar12,
       intro:
         '混音师，录音师，制作人。 2020届美国伯克利音乐学院录音工程专业优秀毕业生。性格开朗，有耐心。现为多次格莱美获奖制作人，混音师Josh Gudwin的工程师，同时为Justin BieberVocal Engineer。合作的艺人有Justin Bieber, Dua Lipa, J Balvin, Bad Bunny, BTS等。 教授高级混音课程',
     },
     {
       name: 'JH',
-      title: ' ',
+      title: '',
       image: avatar13,
       intro:
         '美韩籍作曲家，编曲人，影视配乐家。美国伯克利音乐学院电影配乐专业毕业生，主修乐器吉他，擅⻓几乎所有类型音乐，为许多影视进行过配乐，发表过自己的个人专辑，多年教学经验，同时也是多个乐队的主吉他手，指导过多位不同国籍的学生考入伯克利音乐学院。',
     },
     {
       name: 'Yohei',
-      title: ' ',
+      title: '',
       image: avatar14,
       intro:
         '日裔音乐家，京都出生，音乐家，艺术家。毕业于美国伯克利音乐学院，师从大师 BillyKilson。擅⻓爵士，流行，funk等音乐类型，目前是三个乐队的成员，常年在日本和美国的大小舞台演出，曾在Boston进行多次商演，在校期间非常有名，是一名实力得到所有教授和同学认可的鼓手，具备自己的独树一帜的演奏⻛格。',
     },
     {
       name: 'D哥',
-      title: ' ',
+      title: '',
       image: avatar15,
       intro:
         '男中音歌唱家，博士毕业于德国科隆音乐与舞蹈学院声乐专业，精通美声演唱技巧、舞台表演，声乐艺术，多种语言演唱指导。签约于德国亚琛歌剧院、德国波恩歌剧院、法兰克福Cantate音乐厅、卢森堡音乐厅等。曾获德国瓦格纳奖学金、世界华人声乐大赛首奖及最佳歌剧角色诠释奖等奖项。也是17年德国科隆国际声乐大赛中唯一入围决赛的中国选手。',
@@ -128,6 +122,32 @@ export const teamMembers = {
       title: '',
       image: avatar16,
       intro: 'YZ，制作人，毕业于伯克利音乐学院，电子音乐制作与设计专业，主修乐器大提琴。在音乐制作及音效设计领域均有丰富经验，为独立电影《Departure of Light》配乐及音效制作，参与电子音乐人Terry Zhong《8PM》专辑制作。曾在奥地利维也纳第四届“至高荣耀”国际音乐节等大提琴演奏比赛中获金奖。在范玮琪个人专辑发布会、理查德·克莱德曼的全球巡演中担任大提琴首席。'
+    },
+    {
+      name: 'Kevin',
+      title: '',
+      image: avatar17,
+      intro: 'Kevin, 中国青年鼓手。 高中随父母移居美国后，开始对爵士乐产生浓厚的兴趣。在2017年考入伯克利音乐学院，师从世界级的爵士鼓演奏家与教育家Bob Gullotti, Ralph Peterson, Billy Kilson和Francisco Mela。2019年考入伯克利音乐学院Global Jazz Insitute（全球爵士学院），并成为第一个考上此机构的中国本科生。毕业后移居北京，活跃于国内各大演出场所，与国内顶尖音乐家合作。2021年参加JZ爵士高手乐坛争霸，取得个人与乐队组双冠军。常年从事爵士鼓教学。'
+    },
+    {
+      name: 'Yixin',
+      title: '',
+      image: avatar18,
+      intro: 'Yixin，吉他演奏家，本科伯克利音乐学院。擅长Jazz，Blues，Funk，R&B等多种音乐风格。央音全国青少年评委，奥林匹克青少年音乐展演评委。2020年为全运会北京花样游泳队录制音乐，2022年受邀参加电视台春晚演出。长期持续地在音乐教学领域输出。'
+    },
+    {
+      name: 'MC',
+      title: '',
+      image: avatar2,
+      intro:
+        '毕业于伯克利音乐学院，获半额奖学金，音乐商业管理专业，主修乐器打击乐。参办那英、Twins、GAI周延、李云迪、理查德·克莱德曼等多个艺人的北美巡演活动，在艺人/演出经纪方面有丰富经验。在GOCAA全球钢琴大赛中担任波士顿地区行政负责人。也曾于中央芭蕾舞团交响乐团、国家大剧院交响乐团打击乐声部中参演多场交响乐、芭蕾舞剧，及歌剧演出。“Love what you do，and do what you love.”',
+    },
+    {
+      name: 'Leng Leng',
+      title: '',
+      image: avatar3,
+      intro:
+        '美国伯克利音乐学院爵士钢琴表演专业。致力于探索流行乐、爵士乐、 city pop、与电子⻛格音乐的融合。师从爵士乐大师Yoko Miwa、Doug Johnson、等。结合自由的风格去帮助所有热爱音乐的学生。',
     }
   ],
 };
